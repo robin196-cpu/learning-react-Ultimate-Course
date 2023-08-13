327739f3b62c951429f08d27a7b0a35bb2d7fb92
Md. Robin <robinhosan75@gmail.com>
2023-08-13 15:49:43 +0600

    tutorial number 73 complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Profile from "./components/Profile/Profile";
+// import Profile from "./components/Profile/Profile";
 import TravelList from "./components/travel-list/TravelList";
 function App() {
   return (
--- src/components/travel-list/TravelList.jsx
+++ src/components/travel-list/TravelList.jsx
@@ -22,20 +22,35 @@ function Logo() {
   return <h1>🏝️ Far Away 🧳</h1>;
 }
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
   function handleSubmit(e) {
     e.preventDefault();
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+    setDescription("");
+    setQuantity(1);
   }
+
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        onChange={(e) => setDescription(e.target.value)}
+        value={description}
+      />
       <button>Add</button>
     </form>
   );
